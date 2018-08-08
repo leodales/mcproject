50f10a5f99c8ed4544ca2a5e7486bfbcd0fa21a0
Devin Tan Wei Jie <TPRJCSDTWJ@TP-DOMAIN.LOCAL>
2018-08-08 16:49:32 +0800

    hi

--- app/Http/Controllers/SortController.php
+++ app/Http/Controllers/SortController.php
@@ -12,8 +12,11 @@ use File;
 use Illuminate\Support\Facades\Auth;
 class SortController extends Controller
 {
+     private $arr = array();
+
     public function __construct()
     {
+        
         $this->middleware('auth');
     }
 	public function index()
@@ -30,38 +33,65 @@ class SortController extends Controller
         $this->validate($request, array(
 			'file'      => 'required'
         ));
+        
         if($request->hasFile('file')){
 			$extension = File::extension($request->file->getClientOriginalName());
 			if ($extension == "xlsx" || $extension == "xls" || $extension == "csv") {
                 
-				 $path = $request->file->getRealPath();
-                 Excel::filter('chunk')->load($path)->chunk( 100, function($results)
-                 {
-                     $i = 1;
-                    foreach($results as $row){
-                        $i++;
+                
+                $path = $request->file->getRealPath();
+            
+                $data = Excel::load($path, function($reader) use($path) {
+                    $fulldataset = array();
+                    $returnval = array();
+                    $firstrow = $reader->first()->toArray();
+                    // $keys = explode("0",$keys);
+                    // $keys = implode(' ' ,array_keys($firstrow));
+                    foreach ($firstrow as $key => $value) {
+                        if($key != '0'){
+                            $returnval[] =  $key;
+                        }
                         
                     }
-                    echo ($i);
-                 });
-				// $data = Excel::load($path, function($reader) {
-                //     $firstrow = $reader->first()->toArray();
-                //     if(isset($firstrow['zip']) || isset($firstrow['Postal code'])|| isset($firstrow['pos code'])|| isset($firstrow['Postal Code'])||isset($firstrow['sort'])){
-                //         $rows = $reader->all();
-                //         print_r ($rows[0]);
-                //         // foreach($rows as $row) {
-                //         //     print_r($rows);
-                //         // }
-                //     }else{
-                //         Session::flash('error', 'Different file format..');
-				// 		echo "hi";	
-				// 	   // return back(); 
-                //     }
+                  
+                    print_r($returnval);
+                   
+                    // $keys = array_values($keys);
+                    // print_r($keys); 
+                   // $keys = implode(','array_values($keys));
+                    //echo $keys;
+                    if(isset($firstrow['zip']) || isset($firstrow['Postal code'])|| isset($firstrow['pos code'])|| isset($firstrow['Postal Code'])||isset($firstrow['sort'])){
+                        $rows = $reader->all();
+                        
+                        Excel::filter('chunk')->load($path)->chunk(250, function($results) use($fulldataset)
+                        {
+                         
+                           foreach($results as $row){
+                                array_push($fulldataset, $row);
+                               
+                           }
+                          //echo sizeof($fulldataset);
+                          
+                        });
+                        
+                    }else{
+                        Session::flash('error', 'Different file format..');
+						//echo "hi";	
+					   // return back(); 
+                    }
         
-                 
+                    
                            
-                //  });
+                  });
+              
+			
                 }
          }
+        
     }//end of import
+
+    public function show($arr2){
+        array_push($this->arr, $arr2);
+        echo sizeof($this->arr);
+    }
 }
